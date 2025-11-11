e78e2cf8abf83e5c3a371f3af89509b6d8d7a3c9
Athenkosi-MD <156293161+Athenkosi-MD@users.noreply.github.com>
2025-11-11 10:47:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Nicolaas24@localhost:5432/Unitrans_Dashboard"
+    SQLALCHEMY_DATABASE_URI = "postgresql://edge_admin:OlU01)vX0B!@173.249.46.82:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
